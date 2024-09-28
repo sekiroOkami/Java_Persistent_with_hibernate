4a5af1cb0b6cfb38bc8117485bd573119475cc93
Sekiro <akaraponestonsomboon@gmail.com>
2024-09-28 17:37:03 +0700

    save

--- jpa_2034_c16_e1/src/main/java/com/example/jpa_2034_c16_e1/entities/Author.java
+++ jpa_2034_c16_e1/src/main/java/com/example/jpa_2034_c16_e1/entities/Author.java
@@ -6,7 +6,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 @Entity
-public class Authors {
+public class Author {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private int id;
--- jpa_2034_c16_e1/src/main/java/com/example/jpa_2034_c16_e1/repositories/AuthorsRepository.java
+++ jpa_2034_c16_e1/src/main/java/com/example/jpa_2034_c16_e1/repositories/AuthorsRepository.java
@@ -1,2 +1,23 @@
-package com.example.jpa_2034_c16_e1.repositories;public interface AuthorsRepository {
+package com.example.jpa_2034_c16_e1.repositories;
+
+import com.example.jpa_2034_c16_e1.entities.Author;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Optional;
+
+@Component
+public interface AuthorsRepository extends CrudRepository<Author, Integer> {
+//    @Query("""
+//            SELECT a FROM Author a WHERE a.id= :id
+//            """)
+//    Optional<Author> findAuthorById(Integer id);
+//
+//    @Query("""
+//            SELECT a FROM Author a
+//            """)
+//    List<Author> findAllAuthor(Integer id);
 }
